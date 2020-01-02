452e2aa928c9401c5cfda942a06bbcf253722b07
Mychal Estalilla <mychal.es@protonmail.com>
2020-01-01 23:50:37 -0800

    scaffold consumer service

---
+++ app.js
@@ -0,0 +1,7 @@
+//const db = require('./db');
+
+module.exports = {
+  trecs: msg => {
+    console.log(msg.content.toString())
+  }
+}
--- worker.js
+++ worker.js
@@ -1,11 +1,16 @@
 const amqp = require('amqplib/callback_api');
-const CONN_URL = require('./config/urls.js');
+const CONN_URL = require('./config/urls.js').cloudamqp;
+const { trecs } = require('./app');
 
-amqp.connect(CONN_URL, (err, conn) => {
-  conn.createChannel( (err, ch) => {
-	ch.consume('virtualgroove-app', msg => {
-	  console.log('Message: ', msg.content.toString());
-	}, { noAck: true });
+amqp.connect(CONN_URL, function (err, conn) {
+  if (err) {
+	console.log('Error: ', err);
+  };
+  conn.createChannel(function (err, ch) {
+    ch.consume('virtualgroove-app', function (msg) {
+	  console.log('.....');
+	  trecs(msg);
+      },{ noAck: true }
+    );
   });
 });
-
